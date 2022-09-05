0fcd9eda91f02b8d3b4ec4c2629b46afd385c1a1
Shaheer <hmuhammadshaheer99@gmail.com>
2022-09-06 03:31:37 +0500

    Goals controller and routes setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,14 +1,18 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req,res) => {
+
+//Using async? -> When we use mongoose in each of these func to interact with DB we get a promise!
+const getGoals = asyncHandler(async (req,res) => {
    res.status(200).json({message: 'Get Goals'}) 
-}
+})
 
 // @desc    Set Goals
 // @route   POST /api/goals
 // @access  Private
-const setGoals = (req,res) => {
+const setGoals = asyncHandler(async (req,res) => {
     //console.log(req.body) is undefined initially
     //to make req.body work, we have to create a middleware at server.js
     if (!req.body.text) {
@@ -19,21 +23,21 @@ const setGoals = (req,res) => {
 
     }
     res.status(200).json({message: 'Set Goals!'})
- }
+ })
 
 // @desc   Update Goals
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoals = (req,res) => {
+const updateGoals = asyncHandler(async (req,res) => {
     res.status(200).json({message: `Update Goals ${req.params.id}`})
- }
+ })
 
 // @desc    Delete Goals
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoals = (req,res) => {
+const deleteGoals = asyncHandler(async (req,res) => {
     res.status(200).json({message: `Delete Goals ${req.params.id}`})
- }
+ })
 
 
 
