d3a1e10d45ad59f42313337fee68b76c25dc29e8
Shaheer <hmuhammadshaheer99@gmail.com>
2022-10-02 18:21:52 +0500

    REST API for Goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,16 @@
+//we could have used try catch for asyn - but we can use express async handler by downloading it through npm
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../model/goalModel')
 // @desc    Get Goals
 // @route   GET /api/goals
 // @access  Private
 
 //Using async? -> When we use mongoose in each of these func to interact with DB we get a promise!
 const getGoals = asyncHandler(async (req,res) => {
-   res.status(200).json({message: 'Get Goals'}) 
+   const goals = await Goal.find()
+
+   res.status(200).json(goals) 
 })
 
 // @desc    Set Goals
@@ -22,21 +26,46 @@ const setGoals = asyncHandler(async (req,res) => {
           throw new Error('Please add a text field')
 
     }
-    res.status(200).json({message: 'Set Goals!'})
+    const goal = await Goal.create({
+      text: req.body.text
+    })
+
+
+    res.status(200).json(goal)
  })
 
 // @desc   Update Goals
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoals = asyncHandler(async (req,res) => {
-    res.status(200).json({message: `Update Goals ${req.params.id}`})
+   const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+      res.status(400)
+      throw new Error('Goal not Found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+    res.status(200).json(updatedGoal)
  })
 
 // @desc    Delete Goals
-// @route   DELETE /api/goals/:id
+// @route   DELETE /api/g oals/:id
 // @access  Private
 const deleteGoals = asyncHandler(async (req,res) => {
-    res.status(200).json({message: `Delete Goals ${req.params.id}`})
+   const goal = await Goal.findById(req.params.id)
+
+   if (!goal) {
+     res.status(400)
+     throw new Error('Goal not Found')
+   }
+
+  await goal.remove()
+
+
+    res.status(200).json({ id: req.params.id })
  })
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 //keep track of environment variables
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
+connectDB()
 //initialize express
 const app = express()
 //middleware
